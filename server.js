334c72a494aa0a2cf189a7da70bcd7d57918d56a
Stefan Pop <stefanpop999@gmail.com>
2021-07-12 10:35:11 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ fastify.register(require('./routes/engineers'))
 
 const start = async () => {
   try {
-    await fastify.listen(process.env.PORT || 5000)
+    await fastify.listen(process.env.PORT)
   } catch (error) {
     fastify.log.error(error)
     process.exit(1)
